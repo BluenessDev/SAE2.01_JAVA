14f553d2ee412302c26f6bd1bd7e77b7ae1a0625
Cyril TILAN <cyril.tilan@gmail.com>
2023-05-25 18:03:47 +0200

    Ajout d'une methode ExceptionRPG qui nous servira dans un stade plus avancé du developpement

--- src/main/java/main/modele/ExceptionRPG.java
+++ src/main/java/main/modele/ExceptionRPG.java
@@ -3,11 +3,19 @@ package main.modele;
 class ExceptionRPG extends Exception {
     private int codeErreur;
 
+    /**
+     * Constructeur de la classe ExceptionRPG
+     * @param parCodeErreur int, le code d'erreur
+     */
     public ExceptionRPG(int parCodeErreur) {
         super();
         this.codeErreur = parCodeErreur;
     }
 
+    /**
+     * Methode qui retourne le code d'erreur
+     * @return le code d'erreur
+     */
     public int getCodeErreur() {
         return codeErreur;
     }
